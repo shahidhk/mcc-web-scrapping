7bcf20809775048483b7be4e4aa9a4e2357ca6c5
Shahidh K Muhammed <shahidh@hasura.io>
2016-08-23 20:00:58 +0530

    Add documentation and code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# mcc-web-scraping
+# AIPMT 2016 / NEET 2016 Participating Institutions List
+Please find the list here: [NEET Colleges](http://shahidhk.github.io/mcc-web-scrapping/NEET-Colleges.csv)
+
+# mcc-web-scrapping
 Web scrapping NEET AIQ participating colleges list from Medical Counseling Commettee (MCC) website
+
+## Motivation
+The counselling process for AIPMT/NEET 2016 has just began and it is of utmost importance that the student is informed about all the colleges that are available. But, the Medical Counselling Committee (MCC) Website is so badly designed in such a way that the user has to click on each college link and it opens up details in a new window.
+
+So, I decided to write a web scrapper to gather information regarding all institutions.
+
+Since the page loaded information via ajax calls, Selenium webdriver was used to mimic user action and to render the page. Python bindings were used to select and save relevant information to a CSV file.
+
+
+## Disclaimer 
+This data is collected from MCC website (http://www.mcc.nic.in/MCCRes/Institute-Profile). The csv file provided here or the data collected by python code may not contain all the data provided in the website. Also, there is no guarantees made ragarding the correctness of data. I will not be responsible for any decision made on grounds of the data provided here. Please refer to MCC website for final confirmation.
+
+- Data is owned by MCC
+
+## Credits
+- [Selenium - Web Browser Automation](http://www.seleniumhq.org/) 
+- [ChromeDriver - WebDriver for Chrome](https://sites.google.com/a/chromium.org/chromedriver/getting-started)
---
+++ scrape_sel.py
@@ -0,0 +1,88 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import ElementNotVisibleException
+
+colleges = [] 
+ 
+def init_driver():
+    driver = webdriver.Chrome(executable_path='/path/to/chromedriver')
+    driver.wait = WebDriverWait(driver, 5)
+    return driver
+ 
+def lookup(driver):
+    delay = 60 
+    driver.get('http://www.mcc.nic.in/MCCRes/Institute-Profile')
+    time.sleep(1)
+    links = driver.find_elements_by_xpath("""//*/td[2]/span/a""")
+    print len(links)
+    global colleges
+    counter = 0
+    total = len(links)
+    for link in links:
+        driver.find_element_by_xpath("""//*[@id="ctl00_ContentPlaceHolder1_Medical"]/div/span[2]""").click()
+        driver.find_element_by_xpath("""//*[@id="ctl00_ContentPlaceHolder1_Medical"]/div/span[2]""").click()
+        driver.find_element_by_xpath("""//*[@id="ctl00_ContentPlaceHolder1_Medical"]/div/span[2]""").click()
+        driver.find_element_by_xpath("""//*[@id="ctl00_ContentPlaceHolder1_Medical"]/div/span[2]""").click()
+        counter+=1
+        print "Procession link ", counter, " of ", total, "..." 
+        print "Opening ", link.get_attribute('href')
+        try:
+            link.click()
+        except ElementNotVisibleException:
+            driver.find_element_by_xpath("""//*[@id="ctl00_ContentPlaceHolder1_Medical"]/div/span[2]""").click()
+            link.click()
+        time.sleep(2)    
+        driver.switch_to_window(driver.window_handles[1])
+        driver.implicitly_wait(5)
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH, """//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[7]/td[3]/div"""))
+            WebDriverWait(driver, delay).until(element_present)
+            print "Page is ready!"
+            try:
+                college = [
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td[4]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[2]/td[3]/table/tbody/tr[3]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[2]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[3]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[5]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[6]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[7]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[8]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[9]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[10]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[11]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[12]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[13]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[14]/td[3]/div""").text,
+                    driver.find_element_by_xpath("""//*/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[6]/td[3]/table/tbody/tr[15]/td[3]/div""").text
+                ]
+                print "Saving ", college[1]
+                colleges.append(college)
+                write_to_file(college)
+            except NoSuchElementException:
+                print "Error at ", counter
+                pass
+            except Exception, e:
+                print "Error at ", counter
+                pass
+        except TimeoutException:
+            print "Loading took too much time!"
+        driver.close()
+        print "closed"
+        time.sleep(2)
+        driver.switch_to_window(driver.window_handles[0])
+
+def write_to_file(data):
+    with open('colleges.csv', 'a') as f:
+        f.write((",".join([u'"{0}"'.format(x) for x in data]) + '\n').encode('utf8'))
+ 
+if __name__ == "__main__":
+    driver = init_driver()
+    lookup(driver)
+    time.sleep(5)
+    driver.quit()
